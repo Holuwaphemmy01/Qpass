f7fbfa613e768153b9459f925dd32a24ec9324bb
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-06-06 16:07:51 +0100

    Institution page begins

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import {Toaster} from 'react-hot-toast'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Institution from './pages/Institution'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path='/institution' element={<Institution/>}/>
       </Routes>
     </div>
   )
